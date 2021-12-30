72ba894056852aeb65d937c14912c4d53ef3c084
won4885 <2dcoder@naver.com>
2021-12-30 21:33:04 +0900

    Equality Printer Challenge

---
+++ EqualutyPrinter/src/IntEqualityPrinter.java
@@ -0,0 +1,17 @@
+public class IntEqualityPrinter {
+    private static final String INVALID_VALUE = "Invalid Value";
+
+    public static void printEqual(int n1, int n2, int n3) {
+        if (n1 < 0 || n2 < 0 || n3 < 0) {
+            System.out.println(INVALID_VALUE);
+        } else {
+            if (n1 == n2 && n2 == n3) {
+                System.out.println("All numbers are equal");
+            } else if (n1 != n2 && n2 != n3 && n1 != n3) {
+                System.out.println("All numbers are different");
+            } else {
+                System.out.println("Neither all are equal or different");
+            }
+        }
+    }
+}
