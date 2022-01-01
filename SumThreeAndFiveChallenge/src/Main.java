4ce16ce3d19bfaae93a62b0abd3431cd17299d8e
won4885 <2dcoder@naver.com>
2022-01-01 21:05:24 +0900

    Sum 3 and 5 Challenge Solution (was incorrect)

--- SumThreeAndFiveChallenge/src/Main.java
+++ SumThreeAndFiveChallenge/src/Main.java
@@ -1,15 +1,32 @@
 public class Main {
 
     public static void main(String[] args) {
+//        int sum = 0;
+//        for (int i = 1; i <= 1000; i++) {
+//            if ((i % 3 == 0) && (i % 5 == 0)) {
+//                sum += i;
+//                System.out.println("Number: " + i);
+//                if (sum == 5) {
+//                    break;
+//                }
+//            }
+//        }
+
+        int count = 0;
         int sum = 0;
+
         for (int i = 1; i <= 1000; i++) {
             if ((i % 3 == 0) && (i % 5 == 0)) {
+                count++;
                 sum += i;
-                System.out.println("Number: " + i);
-                if (sum == 5) {
-                    break;
-                }
+                System.out.println("Found number = " + i);
+            }
+
+            if (count == 5) {
+                break;
             }
         }
+
+        System.out.println("Sum = " + sum);
     }
 }
