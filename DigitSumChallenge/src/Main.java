2e243427abdbb3eec46192b329c4357240aaa7f6
won4885 <2dcoder@naver.com>
2022-01-02 18:52:39 +0900

    Digit Sum Challenge (Searched)

---
+++ DigitSumChallenge/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(sumDigits(123));
+    }
+
+    public static int sumDigits(int number) {
+        if (number < 10) {
+            return -1;
+        }
+
+        int sum = 0;
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        return sum;
+    }
+}
