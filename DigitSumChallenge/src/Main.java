5b4c1849819e1521fa039dc4d95964efc6889ed0
won4885 <2dcoder@naver.com>
2022-01-02 20:41:42 +0900

    Digit Sum Challenge Solution

--- DigitSumChallenge/src/Main.java
+++ DigitSumChallenge/src/Main.java
@@ -10,11 +10,20 @@ public class Main {
         }
 
         int sum = 0;
-        while (number > 0) {
-            sum += number % 10;
+//        while (number > 0) {
+//            sum += number % 10;
+//            number /= 10;
+//        }
+
+        // 125 -> (125 / 10) = 12 -> (12 * 10) = 120 -> (125 - 120) = 5
+        while (number > 0) { // 숫자를 10으로 나눠야 하기 때문에 0이 되는 순간이 반드시 온다
+            // extract
+            int digit = number % 10;
+            sum += digit;
+
+            //drop
             number /= 10;
         }
-
         return sum;
     }
 }
