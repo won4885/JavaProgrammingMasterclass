52fa57442690eaf3c271c26f0453e2b6e68fb74f
won4885 <2dcoder@naver.com>
2022-05-04 21:55:01 +0900

    Immutable Class Challenge

--- Adventure/src/Location.java
+++ Adventure/src/Location.java
@@ -6,16 +6,20 @@ public class Location {
     private final String description;
     private final Map<String, Integer> exits;
 
-    public Location(int locationID, String description) {
+    public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<String, Integer>();
+        if (exits != null) {
+            this.exits = new HashMap<>(exits);
+        } else {
+            this.exits = new HashMap<>();
+        }
         this.exits.put("Q", 0);
     }
 
-    public void addExit(String direction, int location) {
-        exits.put(direction, location);
-    }
+//    public void addExit(String direction, int location) {
+//        exits.put(direction, location);
+//    }
     public int getLocationID() {
         return locationID;
     }
