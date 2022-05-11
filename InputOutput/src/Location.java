c621161ab99ffe9feeb3b7f9efb7d934a5cc2994
won4885 <2dcoder@naver.com>
2022-05-11 23:22:12 +0900

    Input and Output (Using Buffered) Challenge

--- InputOutput/src/Location.java
+++ InputOutput/src/Location.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Location {
@@ -10,9 +11,9 @@ public class Location {
         this.locationID = locationID;
         this.description = description;
         if (exits != null) {
-            this.exits = new HashMap<>(exits);
+            this.exits = new LinkedHashMap<>(exits);
         } else {
-            this.exits = new HashMap<>();
+            this.exits = new LinkedHashMap<>();
         }
         this.exits.put("Q", 0);
     }
@@ -29,7 +30,7 @@ public class Location {
     }
 
     public Map<String, Integer> getExits() {
-        return new HashMap<String, Integer>(exits);
+        return new LinkedHashMap<>(exits);
     }
 
     protected void addExit(String direction, int location) {
